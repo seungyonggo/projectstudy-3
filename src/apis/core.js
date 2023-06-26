5cd9d96476c3f9a152569aa6875a44a8f5efbca8
seungyonggo <tmddyd432@gmail>
2023-06-26 21:54:52 +0900

    '3'

--- src/apis/core.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_URL + "/movie/upcoming",
-	headers: {
-		Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
-	},
-});
